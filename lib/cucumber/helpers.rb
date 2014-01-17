653d31cdcf131dd22f7f8491ce626c85ba45a0e0
Greg Beech <greg@blinkbox.com>
2014-01-17 14:10:06 +0000

    Merged upstream/master

--- lib/cucumber/helpers.rb
+++ lib/cucumber/helpers.rb
@@ -1,6 +1,8 @@
+require "cucumber/helpers/version"
+require "cucumber/helpers/core_ext"
+
 module Cucumber
   module Helpers
     VERSION = ::File.read(File.join(File.dirname(__FILE__),"../../VERSION"))
-    
   end
 end
